25fff2a722d9ed08c2287606999e6f6deb1b221f
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-25 12:47:13 +0300

    feat: add new feature

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import { View, Text } from 'react-native'
-
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList } from 'react-native'
+import { useRouter } from 'expo-router'
+import { useState } from 'react'
 import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+      <Text style={styles.userName}>Hello David !!</Text>
+      <Text style={styles.welcomeMessage}>Find your perfect job</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput style={styles.searchInput}/>
+        </View>
+      </View>
     </View>
   )
 }

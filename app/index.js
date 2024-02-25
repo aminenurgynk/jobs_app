b0a7d10c2cd5fbe242a3e53056b5d3cf6a758cde
Aminenur Göynük <amine.gynk@gmail.com>
2024-02-25 12:22:40 +0300

    feat: add  pages

--- app/index.js
+++ app/index.js
@@ -12,8 +12,26 @@ const Home = () => {
         <SafeAreaView style={{ flex:1, backgroundColor: COLORS.lightWhite }}>
             <Stack.Screen options={{
                 headerStyle: { backgroundColor: COLORS.lightWhite },
-
+                headerShadowVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.profile} dimension="100%" />
+                ),
+                headerTitle: ""
             }}/>
+            <ScrollView showsVerticalScrollIndicator={false}>
+               <View style={{
+                flex:1,
+                padding: SIZES.medium, 
+               }} />
+
+               <Welcome />
+               <Popularjobs />
+               <Nearbyjobs />
+            </ScrollView>
         </SafeAreaView>
     )
 }
